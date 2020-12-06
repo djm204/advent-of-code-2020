b3060b1a2f4287d16691b4bc18c7a5295d9b0d11
David Mendez <davidm@boldcommerce.com>
2020-12-06 17:42:38 -0600

    day 3 part 1

--- 3/tobboganPath.js
+++ 3/tobboganPath.js
@@ -1,4 +1,39 @@
+const { start } = require('repl');
 const inputParser = require('../helpers/getInput');
 const inputLines = inputParser.getInputFromFile('input.txt');
+const ROW_LENGTH = inputLines[0].length;
+let skierPosition = 0;
 
-console.log(inputLines);
+const getNewSkierPosition = () => {
+    skierPosition += 3;
+
+    if (skierPosition >= ROW_LENGTH) {
+        skierPosition -= ROW_LENGTH;
+    }
+
+    return skierPosition
+}
+
+const moveDown = (slopeRow) => {
+    slopeRow++;
+    getNewSkierPosition();
+}
+
+const isTree = (line, position) => {
+    return line.split('')[position] === '#';
+}
+
+const ski = () => {
+    let treeCount = 0;
+
+    inputLines.forEach((line) => {
+        if(isTree(line, skierPosition)) {
+            treeCount++;
+        }
+        moveDown(line);
+    });
+
+    return treeCount;
+}
+
+console.log(ski());
