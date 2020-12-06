688259c9fa464e92610a24a4aaa2d4f1aabebbe5
David Mendez <davidm@boldcommerce.com>
2020-12-05 22:28:55 -0600

    solution 2

--- 1/solution.js
+++ 1/solution.js
@@ -2,7 +2,7 @@ const numbers = require('./numbers');
 
 let numbersThatEqual2020 = [];
 
-const findNumbersThatEqual2020 = (numList) => {
+const findTwoNumbersThatEqual2020 = (numList) => {
     let currentNumber = numList.shift();
     
     numbersThatEqual2020 = numList.filter(num => num + currentNumber === 2020);
@@ -10,10 +10,34 @@ const findNumbersThatEqual2020 = (numList) => {
     if(numbersThatEqual2020.length > 0) {
         numbersThatEqual2020.push(currentNumber);
     } else {
-        findNumbersThatEqual2020(numList);
+        findTwoNumbersThatEqual2020(numList);
    }
 }
 
-findNumbersThatEqual2020(numbers);
+// findTwoNumbersThatEqual2020(numbers);
 
-console.log(numbersThatEqual2020.reduce((a,b) => a * b));
+// console.log(numbersThatEqual2020.reduce((a,b) => a * b));
+
+const findThreeNumbersThatEqual2020 = (numList, sumToReach) => {
+    numList.sort();
+    numList.forEach((num, index) => {
+        let leftEdge = index + 1;
+        let rightEdge = numList.length - 1;
+        
+        while (leftEdge < rightEdge) {
+            if(num + numList[leftEdge] + numList[rightEdge] == sumToReach) {
+                numbersThatEqual2020.push(num, numList[leftEdge], numList[rightEdge])
+            } else if (num + numList[leftEdge] + numList[rightEdge] < sumToReach) {
+                leftEdge++;
+            } else {
+                rightEdge--;
+            }
+        }
+    })
+
+}
+
+findThreeNumbersThatEqual2020(numbers, 2020);
+
+console.log(numbersThatEqual2020);
+
